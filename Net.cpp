1661c32cf78459d0c2ba5d61cd3c00060441ede4
-h <erfanzare82@yahoo.com>
2022-03-17 14:02:07 +0330

    join cpp

---
+++ Net.cpp
@@ -0,0 +1,105 @@
+#include <vector>
+
+using namespace std;
+
+class Net
+{
+public:
+    Net(const vector<unsigned> &topology);
+    void feedForward(const vector<double> &inputVals);
+    void backProp(const vector<double> &targetVals);
+    void getResults(vector<double> &resultVals) const;
+    double getRecentAverageError(void) const { return m_recentAverageError; }
+
+private:
+    vector<Layer> m_layers; // m_layers[layerNum][neuronNum]
+    double m_error;
+    double m_recentAverageError;
+    static double m_recentAverageSmoothingFactor;
+};
+
+
+double Net::m_recentAverageSmoothingFactor = 100.0;
+
+
+void Net::getResults(vector<double> &resultVals) const
+{
+    resultVals.clear();
+
+    for (unsigned n = 0; n < m_layers.back().size() - 1; ++n) {
+        resultVals.push_back(m_layers.back()[n].getOutputVal());
+    }
+}
+
+void Net::backProp(const vector<double> &targetVals)
+{
+
+    Layer &outputLayer = m_layers.back();
+    m_error = 0.0;
+
+    for (unsigned n = 0; n < outputLayer.size() - 1; ++n) {
+        double delta = targetVals[n] - outputLayer[n].getOutputVal();
+        m_error += delta * delta;
+    }
+    m_error /= outputLayer.size() - 1; // get average error squared
+    m_error = sqrt(m_error); // RMS
+
+    m_recentAverageError =
+            (m_recentAverageError * m_recentAverageSmoothingFactor + m_error)
+            / (m_recentAverageSmoothingFactor + 1.0);
+
+    for (unsigned n = 0; n < outputLayer.size() - 1; ++n) {
+        outputLayer[n].calcOutputGradients(targetVals[n]);
+    }
+
+    for (unsigned layerNum = m_layers.size() - 2; layerNum > 0; --layerNum) {
+        Layer &hiddenLayer = m_layers[layerNum];
+        Layer &nextLayer = m_layers[layerNum + 1];
+
+        for (unsigned n = 0; n < hiddenLayer.size(); ++n) {
+            hiddenLayer[n].calcHiddenGradients(nextLayer);
+        }
+    }
+
+    for (unsigned layerNum = m_layers.size() - 1; layerNum > 0; --layerNum) {
+        Layer &layer = m_layers[layerNum];
+        Layer &prevLayer = m_layers[layerNum - 1];
+
+        for (unsigned n = 0; n < layer.size() - 1; ++n) {
+            layer[n].updateInputWeights(prevLayer);
+        }
+    }
+}
+
+void Net::feedForward(const vector<double> &inputVals)
+{
+    assert(inputVals.size() == m_layers[0].size() - 1);
+
+    for (unsigned i = 0; i < inputVals.size(); ++i) {
+        m_layers[0][i].setOutputVal(inputVals[i]);
+    }
+
+    for (unsigned layerNum = 1; layerNum < m_layers.size(); ++layerNum) {
+        Layer &prevLayer = m_layers[layerNum - 1];
+        for (unsigned n = 0; n < m_layers[layerNum].size() - 1; ++n) {
+            m_layers[layerNum][n].feedForward(prevLayer);
+        }
+    }
+}
+
+Net::Net(const vector<unsigned> &topology)
+{
+    unsigned numLayers = topology.size();
+    for (unsigned layerNum = 0; layerNum < numLayers; ++layerNum) {
+        m_layers.push_back(Layer());
+        unsigned numOutputs = layerNum == topology.size() - 1 ? 0 : topology[layerNum + 1];
+
+        for (unsigned neuronNum = 0; neuronNum <= topology[layerNum]; ++neuronNum) {
+            m_layers.back().push_back(Neuron(numOutputs, neuronNum));
+            cout << "Made a Neuron!" << endl;
+        }
+
+        m_layers.back().back().setOutputVal(1.0);
+    }
+}
+
---
+++ Neuron.cpp
@@ -0,0 +1,119 @@
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+struct Connection
+{
+    double weight;
+    double deltaWeight;
+};
+
+
+class Neuron;
+
+typedef vector<Neuron> Layer;
+
+class Neuron
+{
+public:
+    Neuron(unsigned numOutputs, unsigned myIndex);
+    void setOutputVal(double val) { m_outputVal = val; }
+    double getOutputVal(void) const { return m_outputVal; }
+    void feedForward(const Layer &prevLayer);
+    void calcOutputGradients(double targetVal);
+    void calcHiddenGradients(const Layer &nextLayer);
+    void updateInputWeights(Layer &prevLayer);
+
+private:
+    static double eta;  
+    static double alpha; 
+    static double transferFunction(double x);
+    static double transferFunctionDerivative(double x);
+    static double randomWeight(void) { return rand() / double(RAND_MAX); }
+    double sumDOW(const Layer &nextLayer) const;
+    double m_outputVal;
+    vector<Connection> m_outputWeights;
+    unsigned m_myIndex;
+    double m_gradient;
+};
+
+double Neuron::eta = 0.15;  
+double Neuron::alpha = 0.5;   
+
+
+void Neuron::updateInputWeights(Layer &prevLayer)
+{
+
+    for (unsigned n = 0; n < prevLayer.size(); ++n) {
+        Neuron &neuron = prevLayer[n];
+        double oldDeltaWeight = neuron.m_outputWeights[m_myIndex].deltaWeight;
+
+        double newDeltaWeight =
+
+        neuron.m_outputWeights[m_myIndex].deltaWeight = newDeltaWeight;
+        neuron.m_outputWeights[m_myIndex].weight += newDeltaWeight;
+    }
+}
+
+double Neuron::sumDOW(const Layer &nextLayer) const
+{
+    double sum = 0.0;
+
+
+    for (unsigned n = 0; n < nextLayer.size() - 1; ++n) {
+        sum += m_outputWeights[n].weight * nextLayer[n].m_gradient;
+    }
+
+    return sum;
+}
+
+void Neuron::calcHiddenGradients(const Layer &nextLayer)
+{
+    double dow = sumDOW(nextLayer);
+    m_gradient = dow * Neuron::transferFunctionDerivative(m_outputVal);
+}
+
+void Neuron::calcOutputGradients(double targetVal)
+{
+    double delta = targetVal - m_outputVal;
+    m_gradient = delta * Neuron::transferFunctionDerivative(m_outputVal);
+}
+
+double Neuron::transferFunction(double x)
+{
+
+    return tanh(x);
+}
+
+double Neuron::transferFunctionDerivative(double x)
+{
+    return 1.0 - x * x;
+}
+
+void Neuron::feedForward(const Layer &prevLayer)
+{
+    double sum = 0.0;
+
+    for (unsigned n = 0; n < prevLayer.size(); ++n) {
+        sum += prevLayer[n].getOutputVal() *
+                prevLayer[n].m_outputWeights[m_myIndex].weight;
+    }
+
+    m_outputVal = Neuron::transferFunction(sum);
+}
+
+Neuron::Neuron(unsigned numOutputs, unsigned myIndex)
+{
+    for (unsigned c = 0; c < numOutputs; ++c) {
+        m_outputWeights.push_back(Connection());
+        m_outputWeights.back().weight = randomWeight();
+    }
+
+    m_myIndex = myIndex;
+}
