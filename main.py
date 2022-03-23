fb248f40732a41acbd8916909d576fc43707302c
erfan <erfanzare82@yahoo.com>
2022-03-23 05:33:22 +0430

    Classify

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-### Start of importing dependecis
+### Start of importing dependecies
 
 import cv2 as cv
 
@@ -23,301 +23,313 @@ from Resizer import Resize
 
 ###  change able for the each device depend on which camera u want to use
 
+class Ai:
 
-cam = cv.VideoCapture(0)
+    def __init__(self,front = "Images\FRONTONE.png" ,top = "Images\TOPSIDE.png"  , cameraindex = 0):
+        self.front = front
+        self.top = top
+        self.cameraindex = cameraindex
 
+    def forward(self):
 
+        cam = cv.VideoCapture(self.cameraindex)
 
-####  load the images from the images directory in the path
 
 
-TOPSIDE = cv.imread("Images\FRONTONE.png", cv.IMREAD_UNCHANGED)
+        ####  load the images from the images directory in the path
 
 
-frontRing = cv.imread("Images\TOPSIDE.png" , cv.IMREAD_UNCHANGED)
+        TOPSIDE = cv.imread( self.front, cv.IMREAD_UNCHANGED)
 
-hf, wf, cf = TOPSIDE.shape
 
+        frontRing = cv.imread( self.top, cv.IMREAD_UNCHANGED)
 
-# frontRing = Resize(frontRing,100)
+        hf, wf, cf = TOPSIDE.shape
 
-# TOPSIDE = Resize(TOPSIDE,100)
 
+        # frontRing = Resize(frontRing,100)
 
-handp = mp.solutions.hands
+        # TOPSIDE = Resize(TOPSIDE,100)
 
-Hands = handp.Hands()
 
-Connections = handp.HAND_CONNECTIONS
+        handp = mp.solutions.hands
 
+        Hands = handp.Hands()
 
-### drawing utils
+        Connections = handp.HAND_CONNECTIONS
 
-class drawer:
-    def __init__(self ,lst):
-        self.fx = lst[0]
-        self.fy = lst[1]
 
-    def drawer(self, frame):
-        cv.circle(frame ,(self.fx,self.fy),10,(250,150,0),6)
-ptime = 0
+        ### drawing utils
 
+        class drawer:
+            def __init__(self ,lst):
+                self.fx = lst[0]
+                self.fy = lst[1]
 
-while True:
+            def drawer(self, frame):
+                cv.circle(frame ,(self.fx,self.fy),10,(250,150,0),6)
+        ptime = 0
 
-    _, frame = cam.read()
 
-    feedimage = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        while True:
 
-    result = Hands.process(feedimage)
+            _, frame = cam.read()
 
-    ntime = time.time()
+            feedimage = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
-    fps = 1 / (ntime - ptime)
+            result = Hands.process(feedimage)
 
-    ptime = ntime
+            ntime = time.time()
 
-    fps = int(fps)
+            fps = 1 / (ntime - ptime)
 
-    lmlist = LmFinder(feedimage, result)
+            ptime = ntime
 
-    if len(lmlist) != 0:
+            fps = int(fps)
 
-        x0 = lmlist[14][1]
+            lmlist = LmFinder(feedimage, result)
 
-        y0 = lmlist[14][2]
+            if len(lmlist) != 0:
 
-        x1 = lmlist[13][1]
+                x0 = lmlist[14][1]
 
-        y1 = lmlist[13][2]
+                y0 = lmlist[14][2]
 
-        fx4 = lmlist[4][1]
+                x1 = lmlist[13][1]
 
-        fy4 = lmlist[4][2]
+                y1 = lmlist[13][2]
 
-        fx8 = lmlist[8][1]
+                fx4 = lmlist[4][1]
 
-        fy8 = lmlist[8][2]
+                fy4 = lmlist[4][2]
 
-        fx12 = lmlist[12][1]
+                fx8 = lmlist[8][1]
 
-        fy12 = lmlist[12][2]
+                fy8 = lmlist[8][2]
 
-        fx16 = lmlist[16][1]
+                fx12 = lmlist[12][1]
 
-        fy16 = lmlist[16][2]
+                fy12 = lmlist[12][2]
 
-        fx20 = lmlist[20][1]
+                fx16 = lmlist[16][1]
 
-        fy20 = lmlist[20][2]
+                fy16 = lmlist[16][2]
 
-        fingers = [[fx4 , fy4],[fx8 , fy8],[fx12 , fy12],[fx16 , fy16],[fx20 , fy20]]
+                fx20 = lmlist[20][1]
 
-        # paranoid1 = int(np.interp(y0 + 30, [y0, y0 + 50], [5, 15]))
+                fy20 = lmlist[20][2]
 
-        readdx = x0 - x1
+                fingers = [[fx4 , fy4],[fx8 , fy8],[fx12 , fy12],[fx16 , fy16],[fx20 , fy20]]
 
-        readdy = y0 - y1
+                # paranoid1 = int(np.interp(y0 + 30, [y0, y0 + 50], [5, 15]))
 
-        drw = [drawer(i) for i in fingers]
+                readdx = x0 - x1
 
-        drawnigga = [drawer.drawer(i,frame) for i in drw]
+                readdy = y0 - y1
 
+                drw = [drawer(i) for i in fingers]
 
-        hs = x0-50
+                drawnigga = [drawer.drawer(i,frame) for i in drw]
 
-        ws = y0-50
 
+                hs = x0-50
 
-        if 150<fy8 & fy12 & fy16<450:
+                ws = y0-50
 
-            cv.circle(frame, (x0 - readdx, y0 - readdy), 10, (255, 255, 0),1)
 
-            # if x0 & y0 != 0:
-            #     print("state1")
-            #     print(x0 , y0)
-            if 60<ws<430 and 60<hs<590:
+                if 150<fy8 & fy12 & fy16<450:
 
-                pfp = (fx16+fx12)
+                    cv.circle(frame, (x0 - readdx, y0 - readdy), 10, (255, 255, 0),1)
 
-                print(pfp)
+                    # if x0 & y0 != 0:
+                    #     print("state1")
+                    #     print(x0 , y0)
+                    if 60<ws<430 and 60<hs<590:
 
-                intpertor = np.interp(pfp,[1000,200],[100,70])                
+                        pfp = (fx16+fx12)
 
-                intpertor = int(intpertor)
+                        print(pfp)
 
-                frontRing = Resize(frontRing,intpertor)
+                        intpertor = np.interp(pfp,[1000,200],[100,70])
 
+                        intpertor = int(intpertor)
 
-                ffr = overlayPNG(frame[y0-20:y0-20+intpertor ,x0-20:x0-20+intpertor,:] ,frontRing)
+                        frontRing = Resize(frontRing,intpertor)
 
 
-                frame[y0-20:y0-20+intpertor ,x0-20:x0-20+intpertor,:] = ffr
+                        ffr = overlayPNG(frame[y0-20:y0-20+intpertor ,x0-20:x0-20+intpertor,:] ,frontRing)
 
-                # [ws:ws+100 ,hs:hs+100,:]
 
-                print("IN TarGEt !")
+                        frame[y0-20:y0-20+intpertor ,x0-20:x0-20+intpertor,:] = ffr
 
-                # print(f"frame : {type(frame[ws:ws+100 ,hs:hs+100,:])} , {frame[ws:ws+100 ,hs:hs+100].shape}")
+                        # [ws:ws+100 ,hs:hs+100,:]
 
+                        print("IN TarGEt !")
 
-                # print(f"FrontRing : {type(frontRing)} , {frontRing.shape}")
+                        # print(f"frame : {type(frame[ws:ws+100 ,hs:hs+100,:])} , {frame[ws:ws+100 ,hs:hs+100].shape}")
 
-                # except:
 
-                #     raise Exception("BRakeAt Point 2")
-        elif fx20>fx12>fx4 and fy4>fy8 :
+                        # print(f"FrontRing : {type(frontRing)} , {frontRing.shape}")
 
-            cv.ellipse(
-                frame,
-                (x0 , y0+20),
-                (30,10),
-                0,
-                0,
-                200,
-                (255,200,0),
-                1)
+                        # except:
 
-            print("Left")
-            
-            
-            
-            
-            
-            if 60<ws<430 and 60<hs<590:
-            
-                pfp = (fx16+fx12)
+                        #     raise Exception("BRakeAt Point 2")
+                elif fx20>fx12>fx4 and fy4>fy8 :
 
-                print(pfp)
+                    cv.ellipse(
+                        frame,
+                        (x0 , y0+20),
+                        (30,10),
+                        0,
+                        0,
+                        200,
+                        (255,200,0),
+                        1)
 
-                intpertor = np.interp(pfp,[1300,200],[100,70])                
+                    print("Left")
 
-                intpertor = int(intpertor)
 
-                TOPSIDE = Resize(TOPSIDE,intpertor)
 
 
-                ffr = overlayPNG(frame[y0+30:y0+30+intpertor ,x0-50:x0-50+intpertor,:] ,TOPSIDE)
 
+                    if 60<ws<430 and 60<hs<590:
 
-                frame[y0+30:y0+30+intpertor ,x0-50:x0-50+intpertor,:] = ffr
+                        pfp = (fx16+fx12)
 
-                # [ws:ws+100 ,hs:hs+100,:]
+                        print(pfp)
 
-                print("IN TarGEt !")
-                
-                
-                # rotation = calculate_rotation(x0-50,fx16)
-                
-                # print(rotation)
+                        intpertor = np.interp(pfp,[1300,200],[100,70])
 
+                        intpertor = int(intpertor)
 
-                # print(f"frame : {type(frame[ws:ws+100 ,hs:hs+100,:])} , {frame[ws:ws+100 ,hs:hs+100].shape}")
+                        TOPSIDE = Resize(TOPSIDE,intpertor)
 
 
-                # print(f"TOPSIDE : {type(TOPSIDE)} , {TOPSIDE.shape}")
+                        ffr = overlayPNG(frame[y0+30:y0+30+intpertor ,x0-50:x0-50+intpertor,:] ,TOPSIDE)
 
-                # except:
 
-                #     raise Exception("BRakeAt Point 2")
+                        frame[y0+30:y0+30+intpertor ,x0-50:x0-50+intpertor,:] = ffr
 
-            print("LEft")
+                        # [ws:ws+100 ,hs:hs+100,:]
 
-     
+                        print("IN TarGEt !")
 
-            """
-                Detection on The LEft Nigga Hands
-            """
 
-        elif fx20<fx12<fx4 and fy4>fy8 :
+                        # rotation = calculate_rotation(x0-50,fx16)
 
-            cv.ellipse(
-                frame,
-                (x0 , y0+20),
-                (30,10),
-                0,
-                0,
-                200,
-                (255,200,0),
-                1)
+                        # print(rotation)
 
-            if 60<ws<430 and 60<hs<590:
-        
-                pfp = (fx16+fx12)
 
-                print(pfp)
+                        # print(f"frame : {type(frame[ws:ws+100 ,hs:hs+100,:])} , {frame[ws:ws+100 ,hs:hs+100].shape}")
 
-                intpertor = np.interp(pfp,[1300,200],[100,70])                
 
-                intpertor = int(intpertor)
+                        # print(f"TOPSIDE : {type(TOPSIDE)} , {TOPSIDE.shape}")
 
-                TOPSIDE = Resize(TOPSIDE,intpertor)
+                        # except:
 
+                        #     raise Exception("BRakeAt Point 2")
 
-                ffr = overlayPNG(frame[y0+30:y0+30+intpertor ,x0-50:x0-50+intpertor,:] ,TOPSIDE)
+                    print("LEft")
 
 
-                frame[y0+30:y0+30+intpertor ,x0-50:x0-50+intpertor,:] = ffr
 
-                # [ws:ws+100 ,hs:hs+100,:]
+                    """
+                        Detection on The LEft Nigga Hands
+                    """
 
-                print("IN TarGEt !")
+                elif fx20<fx12<fx4 and fy4>fy8 :
 
-                # print(f"frame : {type(frame[ws:ws+100 ,hs:hs+100,:])} , {frame[ws:ws+100 ,hs:hs+100].shape}")
+                    cv.ellipse(
+                        frame,
+                        (x0 , y0+20),
+                        (30,10),
+                        0,
+                        0,
+                        200,
+                        (255,200,0),
+                        1)
 
+                    if 60<ws<430 and 60<hs<590:
 
-                # print(f"TOPSIDE : {type(TOPSIDE)} , {TOPSIDE.shape}")
+                        pfp = (fx16+fx12)
 
-                # except:
+                        print(pfp)
 
-                #     raise Exception("BRakeAt Point 2")
+                        intpertor = np.interp(pfp,[1300,200],[100,70])
 
-                # rotation = calculate_rotation(x0-50,fx16)
-                
-                # print(rotation)
+                        intpertor = int(intpertor)
 
-            print("Right")
+                        TOPSIDE = Resize(TOPSIDE,intpertor)
 
-            """
-            Detection on The Right Nigga Hands
-            """
 
+                        ffr = overlayPNG(frame[y0+30:y0+30+intpertor ,x0-50:x0-50+intpertor,:] ,TOPSIDE)
 
-        # print(f"lmList : {lmlist}")
 
-        # print(f"X,Y : {x0,y0}")
+                        frame[y0+30:y0+30+intpertor ,x0-50:x0-50+intpertor,:] = ffr
 
-    cv.putText(frame, f"Fps : {fps}", (60, 60), cv.FONT_ITALIC, 1, (200, 0, 0))
+                        # [ws:ws+100 ,hs:hs+100,:]
 
-    cv.imshow("Test", frame)
-    
-    
-    cv.waitKey(1)
+                        print("IN TarGEt !")
 
-    if cv.waitKey(1) == ord("q"):
+                        # print(f"frame : {type(frame[ws:ws+100 ,hs:hs+100,:])} , {frame[ws:ws+100 ,hs:hs+100].shape}")
 
-        break
 
+                        # print(f"TOPSIDE : {type(TOPSIDE)} , {TOPSIDE.shape}")
 
+                        # except:
 
-# MIT License
+                        #     raise Exception("BRakeAt Point 2")
 
-# Copyright (c) 2022 Erano-
+                        # rotation = calculate_rotation(x0-50,fx16)
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+                        # print(rotation)
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+                    print("Right")
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+                    """
+                    Detection on The Right Nigga Hands
+                    """
+
+
+                # print(f"lmList : {lmlist}")
+
+                # print(f"X,Y : {x0,y0}")
+
+            cv.putText(frame, f"Fps : {fps}", (60, 60), cv.FONT_ITALIC, 1, (200, 0, 0))
+
+            cv.imshow("Test", frame)
+
+
+            cv.waitKey(1)
+
+            if cv.waitKey(1) == ord("q"):
+
+                break
+
+
+
+        # MIT License
+
+        # Copyright (c) 2022 Erano-
+
+        # Permission is hereby granted, free of charge, to any person obtaining a copy
+        # of this software and associated documentation files (the "Software"), to deal
+        # in the Software without restriction, including without limitation the rights
+        # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        # copies of the Software, and to permit persons to whom the Software is
+        # furnished to do so, subject to the following conditions:
+
+        # The above copyright notice and this permission notice shall be included in all
+        # copies or substantial portions of the Software.
+
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+
+
+if __name__ == "__main__":
+    Ai().forward()
